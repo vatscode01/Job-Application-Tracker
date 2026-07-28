198acf2842ec7656695bad29b7abbac981be8588
vatscode01 <ayushvats952@gmail.com>
2026-07-28 10:37:31 +0530

    Some modifications

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sqlalchemy as db
+from models import Applicatiions
+from sqlalchemy_utils import database_exits, create_database
 
 print(db.__version__)
 
-db.create_engine(
+engine = db.create_engine(
     "postgresql://postgres:aady_02@localhost:5432/postgres"
 )
 
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+
 print(db.select(Applications))
+
