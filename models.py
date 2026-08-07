063f962b398c8610e1ea04b71166469f869efb70
vatscode01 <ayushvats952@gmail.com>
2026-08-08 00:46:47 +0530

    added /get applications

--- models.py
+++ models.py
@@ -1,32 +1,10 @@
-from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, Date, JSON, text
-import sqlite3, json
+import sqlite3, pandas as pd
 
-engine = create_engine("sqlite:///job_tracker.db", echo = True)
-meta = MetaData()
+conn = sqlite3.connect("job_tracker.db")
 
-applications = Table(
-    "applications",
-    meta,
-    Column('id', Integer, primary_key=True),
-    Column('company', String, nullable=False),
-    Column('role', String),
-    Column('status', String),       #(applied/interview/offer/rejected)
-    Column('date_applied', Date),
-    Column('extracted_skills', JSON),
-    Column('notes', String, nullable=True)      #Default = True
-)
+table_name = "applications"
+data = pd.read_sql_query(f"select * from {table_name};", conn)
 
-meta.create_all(engine)
-conn = engine.connect()
-
-conn.row_factory = sqlite3.Row
-cursor = conn.cursor()
-
-cursor.execute("select * from job_tracker;")
-rows = cursor.fetchall()
-
-data = [dict(row) for row in rows]
-with open("output.json", "w", encoding="utf-8") as json_file:
-    json.dump(data, json_file, indent=4)
+data.to_json("outputs.json", orient='records', indent=4)
 
 conn.close()
