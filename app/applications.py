14f4d369bb0d13fbaca823613c59595489e30697
vatscode01 <ayushvats952@gmail.com>
2026-08-27 02:30:37 +0530

    Need to work on form option

--- app/applications.py
+++ app/applications.py
@@ -2,21 +2,30 @@ import streamlit as st
 import pandas as pd
 from database_main import print_applications
 
-# df = pd.DataFrame(print_applications(True),index=False)
 df = print_applications(True)
-print(df)
 df = pd.DataFrame(df)
 
 df.columns=['id','company', 'role', 'date_applied', 'extracted_skills', 'deadline', 'status', 'notes']
 df.drop(columns=['id'],inplace=True)
 
+st.title("Job Application Tracker")
 
-print(df)
+with st.form('application_data'):
+    company = st.text_input(label = 'Company Name', placeholder='Enter a valid input')
+    submitted = st.form_submit_button("Submit",)
+    if(submitted):
+        st.write(f"Company: {company}")
 
-st.title("This is a title")
-
+# Show Table
 st.dataframe(
     data = df,
-    hide_index=True
+    hide_index=True,
+    column_config={
+        'company':'Company Name',
+        'role':'Job Role',
+        'date_applied':'Date Applied',
+        'extracted_skills':'Extracted Skills',
+        'deadline':'Deadline'
+    }
 )
 
