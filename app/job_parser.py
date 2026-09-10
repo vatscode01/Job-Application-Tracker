90538330d736e11da9bbca6a70c6896019cbc728
vatscode01 <ayushvats952@gmail.com>
2026-09-10 23:56:43 +0530

    Added job_parser.py

---
+++ app/job_parser.py
@@ -0,0 +1,10 @@
+
+
+skill_directory = ["Python", "Java", "JavaScript", "SQL", "Data Structures & Algorithms", "Object-Oriented Programming", "System Design", "REST APIs", "Git", "Cloud Computing", "Docker", "Kubernetes", "CI/CD", "Statistics", "Machine Learning", "Deep Learning", "Data Visualization", "Pandas", "NumPy", "Scikit-learn", "TensorFlow", "PyTorch", "Power BI", "Tableau", "Data Cleaning", "Data Preprocessing", "Feature Engineering", "NLP", "Computer Vision", "Generative AI", "LLMs", "Transformers", "MLOps", "Model Deployment"]
+
+skill_dictionary = {
+    'Software Role' : ["Data Structures & Algorithms", "Object-Oriented Programming", "Java", "Python", "JavaScript", "SQL", "REST APIs", "Git", "System Design", "Cloud Computing", "Docker", "Kubernetes", "CI/CD"],
+    'Data Science' : ["Python", "SQL", "Statistics", "Machine Learning", "Data Visualization", "Pandas", "NumPy", "Scikit-learn", "Deep Learning", "TensorFlow", "PyTorch", "Power BI", "Tableau", "Data Cleaning", "Feature Engineering", "NLP"] ,
+    'AI/ML': ["Python", "Machine Learning", "Deep Learning", "TensorFlow", "PyTorch", "Scikit-learn", "NLP", "Computer Vision", "Generative AI", "LLMs", "Transformers", "MLOps", "Data Preprocessing", "Feature Engineering", "Model Deployment"],
+}
+
