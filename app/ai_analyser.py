acde3ec151737da83052cc2cf9427d11076f96fb
vatscode01 <ayushvats952@gmail.com>
2026-09-22 00:23:31 +0530

    Completed AI Analyser

--- app/ai_analyser.py
+++ app/ai_analyser.py
@@ -19,38 +19,20 @@ def parse_resume(job_description):
     resume_file = client.files.upload(file = resume_path)
     jd_file = client.files.upload(file = job_description)
 
-    # interaction = client.interactions.create(
-    #     model = "gemini-3.8-flash",
-    #     input = [
-    #         {
-    #             "type": "file_uri",
-    #             "uri" : jd_file.uri,
-    #             "mime_type": jd_file.mime_type,
-    #         },
-    #         {
-    #             "type": "file_uri",
-    #             "uri": resume_file.uri,
-    #             "mime_type": resume_file.mime_type,
-    #         },
-    #         {
-    #             "type" : "text" ,
-    #             "text" : "Given my resume and a job description. Find all the skillset from this pdf also analyse this from the given job description. Find all the matching skillset with the job description and this resume."
-    #         }
-    #     ]
-    # )
+    with st.spinner(
+      "Analyzing resume and job description with Gemini... Please wait ⏳"
+    ):
 
-    response = client.models.generate_content(
-        model = "gemini-3.5-flash",
-        contents = [
-            jd_file,
-            resume_file,
-            (
-                "Given my resume and a job description. Find all the skillset from this pdf also analyse this from the given job description. Find all the matching skillset with the job description and this resume."
-            )
-        ]
-    )
-    while(not response.text):
-        st.write("Loading")
+        response = client.models.generate_content(
+            model = "gemini-3.5-flash",
+            contents = [
+                jd_file,
+                resume_file,
+                (
+                    "Given my resume and a job description. Find all the skillset from this pdf also analyse this from the given job description. Find all the matching skillset with the job description and this resume."
+                )
+            ],
+        )
     st.write(response.text)
 
 #-----------------------------------
